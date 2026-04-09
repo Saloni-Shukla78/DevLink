7da688373b13e1d6d9662b115724af00cf49bd93
Saloni Shukla <salonishukla875@gmail.com>
2026-04-09 14:46:27 +0530

    create connectionRequest schema and create api to send request

---
+++ backend/src/models/connectionRequest.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose");
+
+const connectionRequestSchema=new mongoose.Schema({
+    toUserId:{
+        type:mongoose.Types.ObjectId,
+    },
+    fromUserId:{
+        type:mongoose.Types.ObjectId,
+    },
+    status:{
+        type:String,
+        enum:{
+            values:["interested","ignored","accepted","rejected"],
+            message:`{VALUE} is correct status.`
+        }
+    },
+
+},
+{
+    timestamps:true,
+});
+connectionRequestSchema.index({fromUserId :1,toUserId:1})
+
+const connectionRequestModel=new mongoose.model("ConnectionRequest",connectionRequestSchema);
+
+module.exports=connectionRequestModel
