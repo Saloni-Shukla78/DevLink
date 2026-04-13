3b914d4965d4432fb5881b1c9e56e28645f5e9de
Saloni Shukla <salonishukla875@gmail.com>
2026-04-13 16:57:37 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser");
+const cors=require("cors");
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -9,6 +12,7 @@ const authRouter=require("./routes/auth");
 const profileRouter=require("./routes/profile");
 const requestRouter=require("./routes/request");
 const userRouter = require("./routes/user");
+
 
 app.use("/",authRouter);
 app.use("/",profileRouter);
