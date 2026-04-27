a27b0f097700d3d0c65b8ee6ba66a6899eaaf30f
Saloni Shukla <salonishukla875@gmail.com>
2026-04-27 19:46:46 +0530

    Add feedSlice and read data

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Base_url } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
-  return (
-    <div>
-      Hello,
-    </div>
-  )
-}
+  const [error, setError] = useState("");
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(Base_url + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      setError(err?.res?.data || "Something went wrong..");
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return <div>Hello,</div>;
+};
 
-export default Feed
+export default Feed;
